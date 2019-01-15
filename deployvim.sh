0237442cf6a18802ba3138b4b088401c113882a0
ccggeo <georgeconnorc@gmail.com>
2019-01-15 13:47:31 +0000

    make color directory

--- deployvim.sh
+++ deployvim.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 bundle=$HOME/.vim/bundle
 vundle=$bundle/Vundle.vim
+colors=$HOME/.vim/colors
 
+rm ~/.vimrc
 cp vimrc ~/.vimrc 
-cp colors ~/.vim/colors
 # Download "VundleVim/Vundle.vim" Vim Plugin Manager
 if [ ! -d $vundle ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git $vundle
+fi
+
+if [ ! -d $colors ]; then
+  mkdir ~/.vim/colors
+  cp  colors/* ~/.vim/colors 
   git clone https://github.com/VundleVim/Vundle.vim.git $vundle
 fi
 
