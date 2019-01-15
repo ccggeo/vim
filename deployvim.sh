33e863c913cfdb35d087f9e6f0e947da0998e8e3
ccggeo <georgeconnorc@gmail.com>
2019-01-15 13:29:18 +0000

    bash script to deploy vimrc

---
+++ deployvim.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+bundle=$HOME/.vim/bundle
+vundle=$bundle/Vundle.vim
+
+cp vimrc ~/.vimrc 
+
+# Download "VundleVim/Vundle.vim" Vim Plugin Manager
+if [ ! -d $vundle ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git $vundle
+fi
+
+# PluginInstall: "VundleVim/Vundle.vim" plugin's install command.
+vim +PluginInstall +GoInstallBinaries +qall < /dev/tty
